de556e02e3742663a9f626c3107fa11343d729d5
cary-sas <61653855+cary-sas@users.noreply.github.com>
2022-08-19 12:43:15 +0800

    Update xray_whatever_uuid.sh

--- xray/xray_whatever_uuid.sh
+++ xray/xray_whatever_uuid.sh
@@ -28,6 +28,9 @@ function install_xray_caddy(){
     apt install -y debian-keyring debian-archive-keyring apt-transport-https
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc
     curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+    apt install -y debian-keyring debian-archive-keyring apt-transport-https
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+    curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
     apt update
     apt install caddy
     caddy_url=https://github.com/lxhao61/integrated-examples/releases	
